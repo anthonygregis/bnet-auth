e08bf816d93c73c7e21c745c08f63383076c46a1
Anthony Gregis <anthony@thedevtron.com>
2020-08-14 19:05:53 -0500

    [Passport Strategy] Adds BNet parameters, and removes bcrypt

--- config/ppConfig.js
+++ config/ppConfig.js
@@ -1,8 +1,9 @@
+require('dotenv').config();
 const passport = require('passport')
 const BnetStrategy = require('passport-bnet').Strategy;
 const db = require('../models')
-var BNET_ID = process.env.BNET_ID
-var BNET_SECRET = process.env.BNET_SECRET
+const BNET_ID = process.env.BNET_ID
+const BNET_SECRET = process.env.BNET_SECRET
 
 //passport serrialize's info to make it easier to login
 passport.serializeUser((user, cb) => {
@@ -19,10 +20,12 @@ passport.deserializeUser((id, cb) => {
 })
 
 passport.use(new BnetStrategy({
-    clientId: BNET_ID,
-    clientSecret: BNET_SECRET
+    clientID: BNET_ID,
+    clientSecret: BNET_SECRET,
+    callbackURL: "https://localhost:3000/auth/bnet/callback",
+    scope: ['wow.profile', 'openid']
 }, (accessToken, refreshToken, profile, cb) => {
-    console.log(accessToken, refreshToken, profile)
+    console.log(`Connecting battle.net user '${profile.battletag}' ('${profile.id}')`)
     cb(null, profile)
     // db.user.findOne({
     //     where: { email }
--- models/user.js
+++ models/user.js
@@ -1,5 +1,4 @@
 'use strict';
-const bcrypt = require('bcrypt');
 const {
   Model
 } = require('sequelize');
@@ -45,25 +44,6 @@ module.exports = (sequelize, DataTypes) => {
     sequelize,
     modelName: 'user',
   });
-  user.addHook('beforeCreate', function(pendingUser) {
-    // hash the password for us
-     let hash = bcrypt.hashSync(pendingUser.password, 12);
-    // set password to the hash 
-    pendingUser.password = hash;
-  });
-
-  user.prototype.validPassword = function(passwordTyped) {
-    let correctPassword = bcrypt.compareSync(passwordTyped, this.password);
-    // return true or false based on correct password
-    return correctPassword;
-  };
-  
-  // remove the password before it get serialized (answer when used)
-  user.prototype.toJSON = function() {
-    let userData = this.get();
-    delete userData.password;
-    return userData;
-  };
 
   return user;
 
