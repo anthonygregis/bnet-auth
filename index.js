25c4328aaf26c4def05742451c8831f0bbfea31c
Anthony Gregis <anthony@thedevtron.com>
2020-08-17 14:15:10 -0500

    [Auction House] Get server token

--- index.js
+++ index.js
@@ -1,11 +1,39 @@
+require('dotenv').config()
 const server = require('./server')
 const db = require('./models')
+const request = require('request')
+const axios = require('axios')
+const BNET_ID = process.env.BNET_ID
+const BNET_SECRET = process.env.BNET_SECRET
+
+var dataString = 'grant_type=client_credentials';
+
+var options = {
+    url: 'https://us.battle.net/oauth/token',
+    method: 'POST',
+    body: dataString,
+    auth: {
+        'user': BNET_ID,
+        'pass': BNET_SECRET
+    }
+};
+
+function callback(error, response, body) {
+    if (!error && response.statusCode == 200) {
+        console.log(body);
+    }
+}
+
+
+
 
 const testAuctionMethod = () => {
+    request(options, callback)
     db.connectedRealm.findAll()
         .then(connRealm => {
             connRealm.forEach(aConRealm => {
-                console.log(aConRealm.auctionHouse)
+                let auctionHouse = aConRealm.auctionHouse
+                axios.get(auctionHouse, )
             })
         })
         .catch(err => {
