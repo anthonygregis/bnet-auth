b652151b210b592893dd5aafa641283ef29c78e1
Anthony Gregis <anthony@thedevtron.com>
2020-08-17 15:44:30 -0500

    [Auction House] Retrieve access_token

--- index.js
+++ index.js
@@ -10,21 +10,21 @@ const BNET_SECRET = process.env.BNET_SECRET
 const getToken = () => {
     exec(`curl -u ${BNET_ID}:${BNET_SECRET} -d grant_type=client_credentials https://us.battle.net/oauth/token`
         , (error, result, metadata) => {
-            console.log(result.parse());
+            console.log(result.access_token)
         });
 }
 
 const testAuctionMethod = () => {
     getToken()
-    db.connectedRealm.findAll()
-        .then(connRealm => {
-            connRealm.forEach(aConRealm => {
-                let auctionHouse = aConRealm.auctionHouse
-            })
-        })
-        .catch(err => {
-            console.log(err)
-        })
+    // db.connectedRealm.findAll()
+    //     .then(connRealm => {
+    //         connRealm.forEach(aConRealm => {
+    //             let auctionHouse = aConRealm.auctionHouse
+    //         })
+    //     })
+    //     .catch(err => {
+    //         console.log(err)
+    //     })
 }
 
 //Start Express
