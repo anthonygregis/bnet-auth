b8ef0c30f05f23871d82ae5df320284e5e6fa022
Anthony Gregis <anthony@thedevtron.com>
2020-08-18 18:44:19 -0500

    [Auction Data] fix connectedRealm counting

--- index.js
+++ index.js
@@ -53,16 +53,16 @@ const testAuctionMethod = () => {
                 let auctionHouse = connRealm[currentRealm].auctionHouse
                 axios.get(`${auctionHouse}&access_token=${access_token}`)
                     .then(results => {
-                        //Get all auction info and put each object into csv
-                        //Load csv file and query that into database
+                        // Get all auction info and put each object into csv
+                        // Load csv file and query that into database
                         status = results.status
                         statusMessage = results.statusText
                         if(status === 200) {
-                            for(let i = 0; i < 2000; i++) {
+                            for(let i = 0; i < 10000; i++) {
                                 insertData(results.data.auctions[i], connRealm[currentRealm])
-                                //Go to next connectedRealm
-                                currentRealm += 1
                             }
+                            //Go to next connectedRealm
+                            currentRealm += 1
                         } else {
                             console.log("Auction House Fetch Failed:", statusMessage)
                         }
