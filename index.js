54a8a3e8f90f55c1e8899f528caa6790a72f01ce
Anthony Gregis <anthony@thedevtron.com>
2020-08-18 13:42:04 -0500

    [Auction House] Testing

--- index.js
+++ index.js
@@ -71,18 +71,18 @@ const testAuctionMethod = () => {
 
                                 // Handle stream events --> data, end, and error
                                 readerStream.on('data', function(chunk) {
-                                    console.log("Chunk:", JSON.parse(chunk))
-                                    console.log("-------------------------")
+                                    console.log("Chunk:", chunk)
                                     // data += chunk;
                                 });
 
-                                // readerStream.on('end',function() {
-                                //     // console.log("Data:", data)
-                                // });
-                                //
-                                // readerStream.on('error', function(err) {
-                                //     console.log(err.stack);
-                                // });
+                                readerStream.on('end',function() {
+                                    // console.log("Data:", data)
+                                    console.log("Done")
+                                });
+
+                                readerStream.on('error', function(err) {
+                                    console.log(err.stack);
+                                });
                             } else {
                                 console.log("Auction House Fetch Failed:", statusMessage)
                             }
