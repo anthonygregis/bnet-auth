bff78a42b38a7489d67469bfc6203e43d08ff668
Anthony Gregis <anthony@thedevtron.com>
2020-08-18 10:58:34 -0500

    [Auction House] Check streams

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 require('dotenv').config()
+fs = require('fs')
 const server = require('./server')
 const db = require('./models')
 const request = require('request')
@@ -24,48 +25,62 @@ const testAuctionMethod = () => {
                 connRealm.forEach(aConRealm => {
                     let auctionHouse = aConRealm.auctionHouse
                     axios.get(`${auctionHouse}&access_token=${access_token}`)
-                        .then(results => {
+                        .then(async results => {
+                            await fs.writeFile(auctionData.js, results.data.auctions, encoding = ('utf8'))
                             status = results.status
                             statusMessage = results.statusText
                             auctionData = results.data.auctions
                             if(status === 200) {
-                                console.log(auctionData.length)
-                                let insertData = (itemListing) => {
-                                    setTimeout(() => {
-                                        db.item.findOrCreate({
-                                            where: {
-                                                id: itemListing.item.id
-                                            }
-                                        })
-                                            .then((wowItem, created) => {
-                                                if (created) {
-                                                    console.log("New item added:", wowItem.id)
-                                                }
-                                                // console.log("Item Data:", itemListing)
-                                                db.pricingData.create({
-                                                    unitPrice: itemListing.unit_price || itemListing.buyout,
-                                                    quantity: itemListing.quantity,
-                                                    itemId: itemListing.item.id
-                                                })
-                                                    .then(pricingData => {
-                                                        pricingData.setConnectedRealm(aConRealm)
-                                                    })
-                                                    .catch(err => {
-                                                        console.log("ERROR:", err)
-                                                    })
-                                            })
-                                            .catch(err => {
-                                                console.log("ERROR:", err)
-                                            })
-                                    }, 1000)
-                                }
-                                for(let i = 0; i <= auctionData.length; i += 100) {
-                                    let auctionSubData = auctionData.slice(0, 100)
-                                    // console.log("Subdata Length:", auctionSubData.length)
-                                    auctionSubData.forEach(itemListing => {
-                                        insertData(itemListing)
-                                    })
-                                }
+                                var readerStream = fs.createReadStream('auctionData.js'); //Create a readable stream
+
+                                readerStream.setEncoding('UTF8'); // Set the encoding to be utf8.
+
+                                readerStream.on('readable', function() {
+                                    while ((chunk=readerStream.read()) != null) {
+                                        data += chunk;
+                                    }
+                                });
+
+                                readerStream.on('end', function() {
+                                    console.log(data)
+                                });
+                                // console.log(auctionData.length)
+                                // let insertData = (itemListing) => {
+                                //     setTimeout(() => {
+                                //         db.item.findOrCreate({
+                                //             where: {
+                                //                 id: itemListing.item.id
+                                //             }
+                                //         })
+                                //             .then((wowItem, created) => {
+                                //                 if (created) {
+                                //                     console.log("New item added:", wowItem.id)
+                                //                 }
+                                //                 // console.log("Item Data:", itemListing)
+                                //                 db.pricingData.create({
+                                //                     unitPrice: itemListing.unit_price || itemListing.buyout,
+                                //                     quantity: itemListing.quantity,
+                                //                     itemId: itemListing.item.id
+                                //                 })
+                                //                     .then(pricingData => {
+                                //                         pricingData.setConnectedRealm(aConRealm)
+                                //                     })
+                                //                     .catch(err => {
+                                //                         console.log("ERROR:", err)
+                                //                     })
+                                //             })
+                                //             .catch(err => {
+                                //                 console.log("ERROR:", err)
+                                //             })
+                                //     }, 1000)
+                                // }
+                                // for(let i = 0; i <= auctionData.length; i += 100) {
+                                //     let auctionSubData = auctionData.slice(0, 100)
+                                //     // console.log("Subdata Length:", auctionSubData.length)
+                                //     auctionSubData.forEach(itemListing => {
+                                //         insertData(itemListing)
+                                //     })
+                                // }
                                 // if (auctionData.length > 0) {
                                 //     auctionData.forEach(itemListing => {
                                 //         insertData(itemListing)
