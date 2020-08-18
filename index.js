c708db1eabb095519bb537ce661cb927330ba909
Anthony Gregis <anthony@thedevtron.com>
2020-08-18 11:06:29 -0500

    [Auction House] Test read streams

--- index.js
+++ index.js
@@ -32,18 +32,23 @@ const testAuctionMethod = () => {
                             auctionData = results.data.auctions
                             if(status === 200) {
                                 var readerStream = fs.createReadStream('auctionData.js'); //Create a readable stream
-                                var data = '';
-                                var chunk;
 
-                                readerStream.on('readable', function() {
-                                    while ((chunk=readerStream.read()) != null) {
-                                        data += chunk;
-                                    }
+                                readerStream.setEncoding('UTF8'); // Set the encoding to be utf8.
+
+                                // Handle stream events --> data, end, and error
+                                readerStream.on('data', function(chunk) {
+                                    data += chunk;
                                 });
 
-                                readerStream.on('end', function() {
-                                    console.log(data)
+                                readerStream.on('end',function() {
+                                    console.log(data);
                                 });
+
+                                readerStream.on('error', function(err) {
+                                    console.log(err.stack);
+                                });
+
+                                console.log("Program Ended");
 
                                 fs.unlink('auctionData.js')
                                 // console.log(auctionData.length)
