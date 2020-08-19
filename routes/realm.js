16df72bb6989f03bedf32579a2a7d76f7ee8139d
Anthony Gregis <anthony@thedevtron.com>
2020-08-19 18:44:37 -0500

    [Realm Info] Show items

--- routes/realm.js
+++ routes/realm.js
@@ -22,8 +22,7 @@ router.get('/:realmSlug', async (req, res) => {
     //Get Items Info
     let mostAvailableItems = await db.sequelize.query(`SELECT DISTINCT(itemId), COUNT(quantity) 'totalQuantity' FROM pricingData WHERE connectedRealmId = ${realmInfo.connectedRealmId} GROUP BY itemId ORDER BY 'totalQuantity' LIMIT 10`, { type: QueryTypes.SELECT })
 
-    res.send(mostAvailableItems)
-    // res.render('realm/index', { realmInfo: realmInfo, mostAvailableItems: mostAvailableItems, pageName: realmInfo.name, pageDescription: realmInfo.name + 's historical marketplace data and most popular items currently.' })
+    res.render('realm/index', { realmInfo: realmInfo, mostAvailableItems: mostAvailableItems, pageName: realmInfo.name, pageDescription: realmInfo.name + 's historical marketplace data and most popular items currently.' })
 })
 
 router.get('/:realmSlug/:itemId', async (req, res) => {
