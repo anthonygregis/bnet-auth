0c58ae69c59f206336e40a4ddead06c15328519a
Anthony Gregis <anthony@thedevtron.com>
2020-08-18 19:33:05 -0500

    [Auction Data] Attempt insert of 25000 rows

--- index.js
+++ index.js
@@ -70,7 +70,7 @@ const testAuctionMethod = () => {
                         status = results.status
                         statusMessage = results.statusText
                         if(status === 200) {
-                            for(let i = 0; i < 10000; i++) {
+                            for(let i = 0; i < 25000; i++) {
                                 insertData(results.data.auctions[i], connRealm[currentRealm])
                             }
                             //Go to next connectedRealm after completing for loop
