20fb1fe74e883a4ee2815ab980427450e51a1339
Anthony Gregis <anthony@thedevtron.com>
2020-08-20 15:58:08 -0500

    [Item Detail] Add middleware

--- routes/realm.js
+++ routes/realm.js
@@ -2,6 +2,7 @@ const express = require('express')
 const router = express.Router()
 const db = require('../models')
 const { Op, QueryTypes } = require("sequelize");
+const isLoggedIn = require('../middleware/isLoggedIn')
 
 router.get('/', (req, res) => {
     db.realm.findAll()
