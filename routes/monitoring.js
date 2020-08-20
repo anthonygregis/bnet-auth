d6d66d8976d58a9bcd34053452ab76a63d584826
Anthony Gregis <anthony@thedevtron.com>
2020-08-20 16:09:45 -0500

    [Monitoring] Add sequelize db connection

--- routes/monitoring.js
+++ routes/monitoring.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const router = express.Router()
+const db = require('../models')
 const passport = require('../config/ppConfig')
 const isLoggedIn = require('../middleware/isLoggedIn')
 
