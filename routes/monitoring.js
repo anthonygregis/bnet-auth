3977b910cd030300230c5d206e14c4060a137e4d
Anthony Gregis <anthony@thedevtron.com>
2020-08-21 17:31:28 -0500

    [Sequelize Query] debugging

--- routes/monitoring.js
+++ routes/monitoring.js
@@ -8,7 +8,7 @@ const isLoggedIn = require('../middleware/isLoggedIn')
 router.get('/', isLoggedIn, async (req, res) => {
     const monitoredItems = await db.sequelize.query('SELECT ' +
                                                         'm.*,' +
-                                                        'p.*, AVG(p.unitPrice) AS averagePrice, AVG(p.quantity) AS averageQuantity' +
+                                                        'p.*, AVG(p.unitPrice), AVG(p.quantity)' +
                                                     'FROM monitoredItems m, pricingData p ' +
                                                     `WHERE m.userId = ${req.user.id} AND p.connectedRealmId = m.connectedRealmId AND p.itemId = m.itemId`)
     // +
