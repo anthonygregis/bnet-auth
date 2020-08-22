db581390698a0053c9121410f64a0cdedd913987
Anthony Gregis <anthony@thedevtron.com>
2020-08-22 02:29:07 -0500

    [isLoggedIn] Fix flash messageg

--- middleware/isLoggedIn.js
+++ middleware/isLoggedIn.js
@@ -1,6 +1,6 @@
 module.exports = (req, res, next) => {
     if (!req.user) {
-        req.flash('Error', 'You must be signed to access that page')
+        req.flash('error', 'You must be signed to access that page')
         res.redirect('/')
     } else {
         next()
