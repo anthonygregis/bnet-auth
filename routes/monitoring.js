8f78e57e6705aa41304f4d944e026f49b08f87b9
Anthony Gregis <anthony@thedevtron.com>
2020-08-21 20:34:30 -0500

    [Monitored Items] Fix sql query

--- routes/monitoring.js
+++ routes/monitoring.js
@@ -12,7 +12,7 @@ const isLoggedIn = require('../middleware/isLoggedIn')
 router.get('/', isLoggedIn, async (req, res) => {
     const monitoredItems = await db.sequelize.query(`
         SELECT *,
-               (SELECT (AVG(unitPrice) / SUM(quantity))
+               (SELECT AVG(unitPrice) / SUM(quantity)
                FROM pricingData 
                WHERE pricingData.itemId = monitoredItems.itemId 
                  AND pricingData.connectedRealmId = monitoredItems.connectedRealmId 
