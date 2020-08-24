d9cc4189236eee1613bb30a34b31ac25b84d8d41
Anthony Gregis <anthony@thedevtron.com>
2020-08-24 13:35:07 -0500

    [Error] 404 page

--- server.js
+++ server.js
@@ -64,6 +64,25 @@ app.use((req, res, next) => {
   next();
 });
 
+app.use(function(req, res, next){
+  res.status(404);
+
+  // respond with html page
+  if (req.accepts('html')) {
+    res.render('404', { url: req.url });
+    return;
+  }
+
+  // respond with json
+  if (req.accepts('json')) {
+    res.send({ error: 'Not found' });
+    return;
+  }
+
+  // default to plain-text. send()
+  res.type('txt').send('Not found');
+});
+
 app.use('/auth', require('./routes/auth'))
 app.use('/realms', require('./routes/realm'))
 app.use('/monitoring', require('./routes/monitoring'))
