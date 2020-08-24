fdb43b78321a7d16c64429766cff97b7f28e8cb8
Anthony Gregis <anthony@thedevtron.com>
2020-08-24 13:40:44 -0500

    [404] debug

--- server.js
+++ server.js
@@ -80,21 +80,8 @@ app.get('/items', async (req, res) => {
 
 app.use(function(req, res, next){
   res.status(404);
+  res.render('404', { url: req.url });
 
-  // respond with html page
-  if (req.accepts('html')) {
-    res.render('404', { url: req.url });
-    return;
-  }
-
-  // respond with json
-  if (req.accepts('json')) {
-    res.send({ error: 'Not found' });
-    return;
-  }
-
-  // default to plain-text. send()
-  res.type('txt').send('Not found');
 });
 
 const port = process.env.PORT || 3000;
